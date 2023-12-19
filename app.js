4de2ebede656e58bb98edddef32f66599356198d
ChitraYesodharan <atcr1996chitra@gmail.com>
2023-12-20 00:32:37 +0530

    ButtonClickApp

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element by its ID
+    var myButton = document.getElementById('myButton');
+  
+    // Add a click event listener to the button
+    myButton.addEventListener('click', function() {
+      // Display an alert when the button is clicked
+      alert('Button Clicked!');
+    });
+  });
+  
